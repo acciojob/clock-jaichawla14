2062fac6047a38541ed2827e369b0904ed689713
jaichawla14 <jaichawla1409@gmail.com>
2025-11-19 12:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function updater() {
+            let timer = document.querySelector("p")
+            const now = new Date()
+            timer.innerText = now.toLocaleString()
+        }
+        updater()
+        setInterval(updater, 1000);
